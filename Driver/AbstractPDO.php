1b80c45a8b9f6319bc791e25f497218a5387b3ba
hunzsig <mzyhaohaoren@qq.com>
2019-07-21 23:49:26 +0800

    trans

--- Driver/AbstractPDO.php
+++ Driver/AbstractPDO.php
@@ -59,11 +59,6 @@ abstract class AbstractPDO extends AbstractDB
      */
     protected $where_table = '';
 
-    /**
-     * 多重嵌套事务处理堆栈
-     */
-    protected $transTrace = 0;
-
 
     /**
      * where 条件类型设置
@@ -283,69 +278,6 @@ abstract class AbstractPDO extends AbstractDB
         }
         $this->parameters = array();
         return $PDOStatement;
-    }
-
-    /**
-     * 开始事务
-     */
-    public function beginTrans()
-    {
-        if ($this->transTrace <= 0) {
-            if ($this->pdo()->inTransaction()) {
-                $this->pdo()->commit();
-            }
-            $this->transTrace = 1;
-        } else {
-            $this->transTrace++;
-            return true;
-        }
-        try {
-            return $this->pdo()->beginTransaction();
-        } catch (PDOException $e) {
-            // 服务端断开时重连一次
-            if ($e->errorInfo[1] == 2006 || $e->errorInfo[1] == 2013) {
-                $this->pdoClose();
-                return $this->pdo()->beginTransaction();
-            } else {
-                throw $e;
-            }
-        }
-    }
-
-    /**
-     * 提交事务
-     */
-    public function commitTrans()
-    {
-        $this->transTrace > 0 && $this->transTrace--;
-        if ($this->transTrace > 0) {
-            return true;
-        }
-        return $this->pdo()->commit();
-    }
-
-    /**
-     * 事务回滚
-     */
-    public function rollBackTrans()
-    {
-        $this->transTrace > 0 && $this->transTrace--;
-        if ($this->transTrace > 0) {
-            return true;
-        }
-        if ($this->pdo()->inTransaction()) {
-            return $this->pdo()->rollBack();
-        }
-        return false;
-    }
-
-    /**
-     * 检测是否在一个事务内
-     * @return bool
-     */
-    public function inTransaction()
-    {
-        return $this->pdo()->inTransaction();
     }
 
     /**
--- Transaction/Transaction.php
+++ Transaction/Transaction.php
@@ -3,6 +3,8 @@
 
 namespace Yonna\Database\Transaction;
 
+use PDOException;
+
 /**
  * 事务
  * Class Transaction
@@ -11,84 +13,74 @@ namespace Yonna\Database\Transaction;
 class Transaction
 {
 
-    /**
-     * @var array
-     */
-    private static $is_record = [];
 
     /**
-     * 记录的数据库类型
-     * @var array
+     * 多重嵌套事务处理堆栈
      */
-    private static $record_db_types = [];
+    protected $transTrace = 0;
+
 
     /**
-     * 记录集
-     * @var array
+     * 开始事务
      */
-    private static $records = [];
-
-    /**
-     * 时间基准
-     * @var array
-     */
-    private static $record_time = 0;
-
-    /**
-     * 添加记录
-     * @param string $dbType
-     * @param string $connect
-     * @param string $record
-     */
-    public static function addRecord(string $dbType, string $connect, string $record)
+    public function beginTrans()
     {
-        if (static::$is_record !== true) {
-            return;
-        }
-        if ($record) {
-            $microNow = 1000 * microtime(true);
-            if (!static::$record_db_types || in_array($dbType, static::$record_db_types)) {
-                static::$records[] = [
-                    'type' => $dbType,
-                    'connect' => $connect,
-                    'query' => $record,
-                    'time' => round($microNow - static::$record_time, 4) . 'ms',
-                ];
+        if ($this->transTrace <= 0) {
+            if ($this->pdo()->inTransaction()) {
+                $this->pdo()->commit();
             }
-            static::$record_time = $microNow;
+            $this->transTrace = 1;
+        } else {
+            $this->transTrace++;
+            return true;
+        }
+        try {
+            return $this->pdo()->beginTransaction();
+        } catch (PDOException $e) {
+            // 服务端断开时重连一次
+            if ($e->errorInfo[1] == 2006 || $e->errorInfo[1] == 2013) {
+                $this->pdoClose();
+                return $this->pdo()->beginTransaction();
+            } else {
+                throw $e;
+            }
         }
     }
 
     /**
-     * 启用记录
-     * @param array | string $dbType
+     * 提交事务
      */
-    public static function enableRecord($dbType = null)
+    public function commitTrans()
     {
-        static::$is_record = true;
-        static::$record_db_types = [];
-        if (is_string($dbType)) {
-            static::$record_db_types[] = $dbType;
-        } else if (is_array($dbType)) {
-            static::$record_db_types = $dbType;
+        $this->transTrace > 0 && $this->transTrace--;
+        if ($this->transTrace > 0) {
+            return true;
         }
-        static::$record_time = 1000 * microtime(true);
+        return $this->pdo()->commit();
     }
 
     /**
-     * 获取记录，获取后自动清空
-     * @return array
+     * 事务回滚
      */
-    public static function getRecord()
+    public function rollBackTrans()
     {
-        $record = [];
-        if (isset(static::$records)) {
-            $record = static::$records;
-            static::$records = [];
-            static::$record_time = 0;
+        $this->transTrace > 0 && $this->transTrace--;
+        if ($this->transTrace > 0) {
+            return true;
         }
-        return $record;
+        if ($this->pdo()->inTransaction()) {
+            return $this->pdo()->rollBack();
+        }
+        return false;
     }
 
+    /**
+     * 检测是否在一个事务内
+     * @return bool
+     */
+    public function inTransaction()
+    {
+        return $this->pdo()->inTransaction();
+    }
 
 }
